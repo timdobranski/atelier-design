a11c4d643503fe51d5b0ad61bf4166d98b808f24
Tim <timdobranski@gmail.com>
2023-09-16 19:02:50 -0700

    Sendgrid emailing works on the back end

--- server.js
+++ server.js
@@ -2,12 +2,36 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3002;
 const path = require('path');
+const sgMail = require('@sendgrid/mail');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-// app.get('/api/hello', (req, res) => {
-//   res.send({ message: 'Hello from server!' });
-// });
 app.use(express.static(path.join(__dirname, './build')));
+app.use(bodyParser.json());
 
+app.post('/send-email', async (req, res) => {
+
+
+  const { recipientEmail } = req.body;
+  console.log('recipient email:', recipientEmail);
+
+  try {
+    const msg = {
+      to: recipientEmail,
+      from: 'timdobranski@gmail.com',
+      subject: 'Welcome to the Family!',
+      text: 'We\'re glad to have you aboard! Stay tuned for more cool update!'
+    };
+
+    await sgMail.send(msg);
+    console.log('Email sent');
+    res.sendStatus(200); // Send a success status back to the React app
+  } catch (error) {
+    console.error('Email sending failed:', JSON.stringify(error));
+    res.status(500).send('Email sending failed'); // Send an error status back to the React app
+  }
+});
 
 
 
